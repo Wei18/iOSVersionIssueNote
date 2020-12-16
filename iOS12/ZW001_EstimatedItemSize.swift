4dabf16f6c71cdde4175ba5fef5b5ec46b0788c8
Wei <41205mw@gmail.com>
2020-12-16 12:39:10 +0800

    Update and rename estimatedItemSize to ZW001_EstimatedItemSize.swift

--- iOS12/ZW001_EstimatedItemSize.swift
+++ iOS12/ZW001_EstimatedItemSize.swift
@@ -1,3 +1,9 @@
+//
+//  ZWEstimatedItemSize.swift
+//
+//  Created by ZhiWei Cao on 2020/12/16.
+//
+
 // For UICollectionView.estimatedItemSize
 // Fix on UICollectionViewCell
 // REF: https://stackoverflow.com/questions/51375566/in-ios-12-when-does-the-uicollectionview-layout-cells-use-autolayout-in-nib/52428617#52428617
